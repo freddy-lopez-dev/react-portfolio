b1b0b51ede0d0e9bc4ca397115bd6048528c3509
Frederick Lopez <mtester010@gmail.com>
2022-08-19 21:01:41 -0500

    Added services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,102 @@
 import React from 'react';
 import './services.css';
+import { BsCheckLg } from 'react-icons/bs';
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF Web dev */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Graphic Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
