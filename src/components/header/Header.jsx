e69b9325bc351fe19d6cc24c6130a622e47eab79
Frederick Lopez <mtester010@gmail.com>
2022-08-19 22:09:42 -0500

    Adjust the 3d model

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Frederick Lopez</h1>
-        <h5 className="text-light">UI/UX Designer / Software Developer</h5>
+        <h5 className="text-light">UI/UX Developer / Software Developer</h5>
         <CTA />
         <Social />
         <div className="me">
